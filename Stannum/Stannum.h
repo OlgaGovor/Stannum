01046ceb52bbf8ae766c1c54f5a0bac4cc87d263
Olga Govor <olga.govor@grandparade.co.uk>
2018-01-24 10:01:11 +0100

    Initial Commit

---
+++ Stannum/Stannum.h
@@ -0,0 +1,19 @@
+//
+//  Stannum.h
+//  Stannum
+//
+//  Created by Olga Govor on 24/01/2018.
+//  Copyright © 2018 Olga Govor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Stannum.
+FOUNDATION_EXPORT double StannumVersionNumber;
+
+//! Project version string for Stannum.
+FOUNDATION_EXPORT const unsigned char StannumVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Stannum/PublicHeader.h>
+
+
