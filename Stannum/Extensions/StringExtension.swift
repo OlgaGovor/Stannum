7e71f3bda0dd245676deff0b795985b26937214f
Olga Govor <olga.govor@grandparade.co.uk>
2018-01-26 14:53:54 +0100

    add switch on/off wifi

--- Stannum/Extensions/StringExtension.swift
+++ Stannum/Extensions/StringExtension.swift
@@ -20,4 +20,12 @@ extension String {
         }
         return randomString
     }
+    
+    public func toBool() -> Bool? {
+        switch self {
+            case "True", "true", "yes", "1": return true
+            case "False", "false", "no", "0": return false
+            default: return nil
+        }
+    }
 }
