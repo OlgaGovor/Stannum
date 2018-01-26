ffa48c9667f80b9e0f86ba8b644f68159e360973
Olga Govor <olga.govor@grandparade.co.uk>
2018-01-26 13:42:28 +0100

    add search by specific Label/text/id

--- Stannum/Extensions/XCUIElementQueryExtension.swift
+++ Stannum/Extensions/XCUIElementQueryExtension.swift
@@ -9,18 +9,33 @@ import XCTest
 
 extension XCUIElementQuery {
     
-    public func labelContains(text: String) -> XCUIElementQuery {
+    public func byLabelContains(text: String) -> XCUIElementQuery {
         let query = self.containing(NSPredicate(format: "label CONTAINS %@", text))
         return query
     }
     
-    public func idContains(text: String) -> XCUIElementQuery {
+    public func byIdContains(text: String) -> XCUIElementQuery {
         let query = self.containing(NSPredicate(format: "identificator CONTAINS %@", text))
         return query
     }
     
-    public func textContains(text: String) -> XCUIElementQuery {
+    public func byTextContains(text: String) -> XCUIElementQuery {
         let query = self.containing(NSPredicate(format: "text CONTAINS %@", text))
+        return query
+    }
+    
+    public func byLabel(text: String) -> XCUIElementQuery {
+        let query = self.containing(NSPredicate(format: "label = %@", text))
+        return query
+    }
+    
+    public func byId(text: String) -> XCUIElementQuery {
+        let query = self.containing(NSPredicate(format: "identificator = %@", text))
+        return query
+    }
+    
+    public func byText(text: String) -> XCUIElementQuery {
+        let query = self.containing(NSPredicate(format: "text = %@", text))
         return query
     }
 }
