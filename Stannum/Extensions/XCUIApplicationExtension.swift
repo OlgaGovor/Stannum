4ff1082ed32a2b0b4b56ea2b8846d3caeaaea296
Olga Govor <olga.govor@grandparade.co.uk>
2018-04-14 18:02:49 +0200

    add docs to XCUIApplication

--- Stannum/Extensions/XCUIApplicationExtension.swift
+++ Stannum/Extensions/XCUIApplicationExtension.swift
@@ -10,12 +10,22 @@ import XCTest
 
 extension XCUIApplication {
     
+    /**
+     Extension to XCUIApplication
+     
+     Active application go to background by click on **Home** button
+     */
     public func goToBackground() {
         XCTContext.runActivity(named: "Application goes to background") { _ in
             XCUIDevice.shared.press(.home);
         }
     }
     
+    /**
+     Extension to XCUIApplication
+     
+     Hard close (terminate) application
+     */
     public func close() {
         XCTContext.runActivity(named: "Application hard close") { _ in
             self.terminate();
